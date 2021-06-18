10113ad68d7a15396f065a7b181e404c603222c8
Ricco <randres@svi.com.ph>
2021-06-18 15:49:24 +0800

    Update Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 // MATERIAL UI
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SubHeaderComponent } from './sub-header/sub-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RoutingComponents
+    RoutingComponents,
+    SubHeaderComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-
     // MATERIAL UI
     MatToolbarModule,
     MatSidenavModule,
-
+    MatCardModule,
+    
+    BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule
     
   ],
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  // ANIMATION FOR SUBHEADER. ILIPAT SA SEPARATE NA FILE FOR REUSABILITY
+  animations: [
+    trigger('popOverState', [
+      state('show', style({
+        opacity: 1
+      })),
+      state('hide', style({
+        opacity: 0
+      })),
+      transition('show => hide', animate('1000ms ease-out')),
+      transition('hide => show', animate('600ms ease-out')),
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
-  showFiller = false;
+  public subMenuClicked = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public get stateName(){
+    return this.subMenuClicked ? 'show' : 'hide'
+  }
+
+  subMenuToggle(){
+    this.subMenuClicked = !this.subMenuClicked;
+  }
+
+ 
 }
